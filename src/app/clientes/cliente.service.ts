2962ce52e58bd76a14a2817c621c337750505125
Exequiel Carrizo <exelcarrizo@gmail.com>
2024-02-22 01:18:15 -0300

    Update,delete de los clientes

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,28 +1,42 @@
-import { Injectable } from '@angular/core';
-import { CLIENTES } from "./clientes.json"; // Importa el listado de clientes desde un archivo JSON (no utilizado actualmente)
-import { Cliente } from "./cliente"; // Importa el modelo Cliente
-import { Observable, of } from "rxjs"; // Importa Observable y of de RxJS
-import { HttpClient, HttpHeaders } from "@angular/common/http"; // Importa HttpClient de Angular para realizar solicitudes HTTP
-import { map } from "rxjs/operators"; // Importa el operador map de RxJS para transformar datos
+import {Injectable} from '@angular/core';
+import {CLIENTES} from "./clientes.json"; // Importa el listado de clientes desde un archivo JSON (no utilizado actualmente)
+import {Cliente} from "./cliente"; // Importa el modelo Cliente
+import {Observable, of} from "rxjs"; // Importa Observable y of de RxJS
+import {HttpClient, HttpHeaders} from "@angular/common/http"; // Importa HttpClient de Angular para realizar solicitudes HTTP
+import {map} from "rxjs/operators"; // Importa el operador map de RxJS para transformar datos
 
 @Injectable({
-  providedIn: 'root' // Define que este servicio se proveerá en el ámbito de toda la aplicación
+    providedIn: 'root' // Define que este servicio se proveerá en el ámbito de toda la aplicación
 })
 export class ClienteService {
-  private urlEndPoint:string = 'http://localhost:8080/api/clientes'; // URL del punto final de la API para clientes
-  private httpHeaders = new HttpHeaders({'Content-Type' : 'application/json'}); // Encabezados HTTP JSON
+    private urlEndPoint: string = 'http://localhost:8080/api/clientes'; // URL del punto final de la API para clientes
+    private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'}); // Encabezados HTTP JSON
 
-  constructor(private http: HttpClient) { } // Constructor que inyecta HttpClient para realizar solicitudes HTTP
+    constructor(private http: HttpClient) {
+    } // Constructor que inyecta HttpClient para realizar solicitudes HTTP
 
-  // Método para obtener la lista de clientes desde el backend
-  getClientes(): Observable<Cliente[]> {
-    return this.http.get<Cliente[]>(this.urlEndPoint).pipe( // Realiza una solicitud GET al servidor usando HttpClient
-      map((response) => response as Cliente[]) // Utiliza el operador map para transformar la respuesta en un array de Clientes
-    );
-  }
+    // Método para obtener la lista de clientes desde el backend
+    getClientes(): Observable<Cliente[]> {
+        return this.http.get<Cliente[]>(this.urlEndPoint).pipe( // Realiza una solicitud GET al servidor usando HttpClient
+            map((response) => response as Cliente[]) // Utiliza el operador map para transformar la respuesta en un array de Clientes
+        );
+    }
 
-  // Método para crear un nuevo cliente en el backend
-  create(cliente: Cliente): Observable<Cliente> {
-    return this.http.post<Cliente>(this.urlEndPoint, cliente, { headers: this.httpHeaders }); // Realiza una solicitud POST al servidor para crear un nuevo cliente
-  }
+    // Método para crear un nuevo cliente en el backend
+    create(cliente: Cliente): Observable<Cliente> {
+        return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders}); // Realiza una solicitud POST al servidor para crear un nuevo cliente
+    }
+
+    getCliente(id): Observable<any> {
+        return this.http.get(`${this.urlEndPoint}/${id}`)
+    }
+
+    update(cliente: Cliente): Observable<any> {
+        return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders})
+    }
+
+    delete(id: number): Observable<any>{
+        return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders})
+    }
+
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,29 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { Cliente } from "./cliente"; // Importa la clase Cliente desde el archivo cliente.ts
-import { ClienteService } from "./cliente.service"; // Importa el servicio ClienteService
-import { Router } from "@angular/router"; // Importa el enrutador de Angular
+import {Component, OnInit} from '@angular/core';
+import {Cliente} from "./cliente"; // Importa la clase Cliente desde el archivo cliente.ts
+import {ClienteService} from "./cliente.service"; // Importa el servicio ClienteService
+import {Router, ActivatedRoute} from "@angular/router"; // Importa el enrutador de Angular
+import swal from 'sweetalert2'
+import {Observable} from "rxjs";
+
 
 @Component({
-  selector: 'app-form', // Selector CSS que identifica este componente
-  templateUrl: 'form.component.html', // Ubicación del archivo HTML asociado a este componente
+    selector: 'app-form', // Selector CSS que identifica este componente
+    templateUrl: 'form.component.html', // Ubicación del archivo HTML asociado a este componente
 })
 export class FormComponent implements OnInit { // Definición de la clase del componente, implementando OnInit
 
-  public cliente: Cliente = new Cliente(); // Instancia de la clase Cliente, inicializada como un nuevo cliente
-  public titulo:string = "Crear Cliente"; // Variable que define el título del formulario
+    public cliente: Cliente = new Cliente(); // Instancia de la clase Cliente, inicializada como un nuevo cliente
+    public titulo: string = "Crear Cliente"; // Variable que define el título del formulario
 
-  constructor(private clienteService: ClienteService, // Constructor del componente, inyecta el servicio ClienteService y el enrutador
-              private router: Router) {
-  }
+    constructor(private clienteService: ClienteService, // Constructor del componente, inyecta el servicio ClienteService y el enrutador
+                private router: Router,
+                private activatedRoute: ActivatedRoute) {
+    }
 
-  ngOnInit() {
-    // Método del ciclo de vida del componente, se ejecuta al inicializar el componente
-  }
+    ngOnInit() {
+        this.cargarCliente()
+        // Método del ciclo de vida del componente, se ejecuta al inicializar el componente
+    }
 
-  public create(): void {
-    // Método para crear un nuevo cliente
-    this.clienteService.create(this.cliente).subscribe( // Llama al método create del servicio ClienteService, pasando el cliente como parámetro
-      response => this.router.navigate(['/clientes']) // Navega a la ruta '/clientes' después de crear exitosamente el cliente
-    )
-  }
+    cargarCliente(): void {
+        this.activatedRoute.params.subscribe(params => {
+            let id = params['id']
+            if (id) {
+                this.clienteService.getCliente(id).subscribe((cliente) => this.cliente = cliente)
+            }
+        })
+    }
+
+    create(): void {
+        // Método para crear un nuevo cliente
+        this.clienteService.create(this.cliente).subscribe( // Llama al método create del servicio ClienteService, pasando el cliente como parámetro
+            cliente => {
+                this.router.navigate(['/clientes'])
+                // @ts-ignore
+                swal.fire('Nuevo Cliente', `Cliente ${cliente.nombre} creado con éxito!`, 'success')
+            } // Navega a la ruta '/clientes' después de crear exitosamente el cliente
+        );
+    }
+
+    update(): void {
+        this.clienteService.update(this.cliente).subscribe(
+            cliente => {
+                this.router.navigate(['/clientes'])
+                swal.fire('Cliente Actualizado', `Cliente ${cliente.nombre} actualizado con éxito!`, 'success')
+            }
+        )
+    }
+
+
 }
