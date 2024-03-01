77926ead0ccf119993e42a19bd820bde3f14232c
Exequiel Carrizo <exelcarrizo@gmail.com>
2024-03-01 12:42:26 -0300

    Update

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -7,52 +7,64 @@ import {Observable} from "rxjs";
 
 
 @Component({
-    selector: 'app-form', // Selector CSS que identifica este componente
-    templateUrl: 'form.component.html', // Ubicación del archivo HTML asociado a este componente
+  selector: 'app-form', // Selector CSS que identifica este componente
+  templateUrl: 'form.component.html', // Ubicación del archivo HTML asociado a este componente
 })
 export class FormComponent implements OnInit { // Definición de la clase del componente, implementando OnInit
 
-    public cliente: Cliente = new Cliente(); // Instancia de la clase Cliente, inicializada como un nuevo cliente
-    public titulo: string = "Crear Cliente"; // Variable que define el título del formulario
+  public cliente: Cliente = new Cliente(); // Instancia de la clase Cliente, inicializada como un nuevo cliente
+  public titulo: string = "Crear Cliente"; // Variable que define el título del formulario
 
-    constructor(private clienteService: ClienteService, // Constructor del componente, inyecta el servicio ClienteService y el enrutador
-                private router: Router,
-                private activatedRoute: ActivatedRoute) {
-    }
+  public errores: string[];
 
-    ngOnInit() {
-        this.cargarCliente()
-        // Método del ciclo de vida del componente, se ejecuta al inicializar el componente
-    }
+  constructor(private clienteService: ClienteService, // Constructor del componente, inyecta el servicio ClienteService y el enrutador
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+  }
 
-    cargarCliente(): void {
-        this.activatedRoute.params.subscribe(params => {
-            let id = params['id']
-            if (id) {
-                this.clienteService.getCliente(id).subscribe((cliente) => this.cliente = cliente)
-            }
-        })
-    }
+  ngOnInit() {
+    this.cargarCliente()
+    // Método del ciclo de vida del componente, se ejecuta al inicializar el componente
+  }
 
-    create(): void {
-        // Método para crear un nuevo cliente
-        this.clienteService.create(this.cliente).subscribe( // Llama al método create del servicio ClienteService, pasando el cliente como parámetro
-            cliente => {
-                this.router.navigate(['/clientes'])
-                // @ts-ignore
-                swal.fire('Nuevo Cliente', `Cliente ${cliente.nombre} creado con éxito!`, 'success')
-            } // Navega a la ruta '/clientes' después de crear exitosamente el cliente
-        );
-    }
+  cargarCliente(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id']
+      if (id) {
+        this.clienteService.getCliente(id).subscribe((cliente) => this.cliente = cliente)
+      }
+    })
+  }
 
-    update(): void {
-        this.clienteService.update(this.cliente).subscribe(
-            cliente => {
-                this.router.navigate(['/clientes'])
-                swal.fire('Cliente Actualizado', `Cliente ${cliente.nombre} actualizado con éxito!`, 'success')
-            }
-        )
-    }
+  create(): void {
+    // Método para crear un nuevo cliente
+    this.clienteService.create(this.cliente).subscribe( // Llama al método create del servicio ClienteService, pasando el cliente como parámetro
+      cliente => {
+        this.router.navigate(['/clientes'])
+        // @ts-ignore
+        swal.fire('Nuevo Cliente', `Cliente ${cliente.nombre} creado con éxito!`, 'success')
+      },
+      err => {
+        this.errores = err.error.errors as string[];
+        console.error('Código del error desde el backend: ' + err.status);
+        console.error(err.error.errors)
+      }
+    );
+  }
+
+  update(): void {
+    this.clienteService.update(this.cliente).subscribe(
+      cliente => {
+        this.router.navigate(['/clientes'])
+        swal.fire('Cliente Actualizado', `Cliente ${cliente.nombre} actualizado con éxito!`, 'success')
+      },
+      err => {
+        this.errores = err.error.errors as string[];
+        console.error('Código del error desde el backend: ' + err.status);
+        console.error(err.error.errors)
+      }
+    );
+  }
 
 
 }
